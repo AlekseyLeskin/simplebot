5590dc2f0b791ff28e202f6f88e00e02ffaf2d74
a.leskin <a.leskin@MacPro>
2018-09-30 10:32:47 +0300

    /start command logging

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
 
 def start_bot(bot, update):
     mytext = "Привет, {}!".format(update.message.chat.first_name)
-    logging.info('Пользователь {} нажал /start'.format(update.message.chat.username))
+    logging.info("Пользователь {} нажал /start".format(update.message.chat.username))
     update.message.reply_text(mytext)
 
 
